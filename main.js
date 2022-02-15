edc1eb5026f6fcaa8de06876c037b01d4ece8d03
rohan-star7308 <87434006+rohan-star7308@users.noreply.github.com>
2022-02-15 19:40:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload(){
-
+mustache=loadImage("https://i.postimg.cc/MTjF7XKH/mustache.jpg");
 }
 
 function setup(){
@@ -14,10 +14,13 @@ function setup(){
 function modelLoaded(){
     console.log("poseNet initialized");
 }
-
+var nosex=0;
+var nosey=0;
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
         console.log('nose x = '+results[0].pose.nose.x);
         console.log('nose y = '+results[0].pose.nose.y);
     }
@@ -25,6 +28,7 @@ function gotPoses(results){
 
 function draw(){
     image(video,0,0,300,300);
+    image(mustache,nosex,nosey,50,30);
 }
  function take_snapshot(){
      save('clown-nose.png');
